afbcd75362645d0317550be6d23b0c4c31b5fcf2
cuong-ich-truong <>
2022-06-28 12:31:14 +0700

    update friend views style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'home#index'
-  get 'home/about'
+  # root 'home#index'
+  root "friends#index"
+  get "home/about"
 end
